3fb0d29625b6342552657ca7594959797e92e197
Alexander <nekon689@gmail.com>
2024-05-12 14:26:48 +0300

    Added commands to the Makefile and connected the jacoco library and configured the linter for the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,10 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
+    jacoco
     id("java")
+    id("checkstyle")
 }
 
 group = "hexlet.code"
@@ -16,4 +21,15 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }

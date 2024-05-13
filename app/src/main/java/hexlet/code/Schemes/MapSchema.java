4f8ca644537b537952d6462691a1638111b10e1b
Alexander <nekon689@gmail.com>
2024-05-13 18:29:49 +0300

    added nested validation

--- app/src/main/java/hexlet/code/Schemes/MapSchema.java
+++ app/src/main/java/hexlet/code/Schemes/MapSchema.java
@@ -1,15 +1,41 @@
 package hexlet.code.Schemes;
 
 import java.util.Map;
+import java.util.HashMap;
 
-public class MapSchema extends BaseSchema<Map<?, ?>> {
-    public MapSchema required() {
+public class MapSchema<V> extends BaseSchema<Map<String, V>> {
+    private Map<String, BaseSchema<V>> schemaShapeMap = new HashMap<>();
+
+    public MapSchema<V> required() {
         validators.put("required", value -> value != null);
         return this;
     }
 
-    public MapSchema sizeof(int size) {
+    public MapSchema<V> sizeof(int size) {
         validators.put("sizeof", value -> value == null || size == value.size());
         return this;
+    }
+
+    public MapSchema<V> shape(Map<String, BaseSchema<V>> validators) {
+        schemaShapeMap = validators;
+        return this;
+    }
+
+    @Override
+    public boolean isValid(Map<String, V> value) {
+        boolean result = super.isValid(value);
+        if (!result || value == null) {
+            return result;
+        }
+
+        for (Map.Entry<String, BaseSchema<V>> entry : schemaShapeMap.entrySet()) {
+            String key = entry.getKey();
+            BaseSchema<V> schema = entry.getValue();
+            if (!schema.isValid(value.get(key))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

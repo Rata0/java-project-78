50b30e3c180b02337389970d4a318d0ae71ecd61
Alexander <nekon689@gmail.com>
2024-05-13 15:24:11 +0300

    BaseSchema<T> base class is created and isValid method is implemented

---
+++ app/src/main/java/hexlet/code/Schemes/BaseSchema.java
@@ -0,0 +1,13 @@
+package hexlet.code.Schemes;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public class BaseSchema<T> {
+    protected Map<String, Predicate<T>> validators = new HashMap<>();
+
+    public boolean isValid(T value) {
+        return validators.values().stream().allMatch(rule -> rule.test(value));
+    }
+}
